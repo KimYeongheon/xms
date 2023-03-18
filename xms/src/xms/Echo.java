0ae14901da83114f79911ae25438b89ee5dae300
KimYeongheon <mmnmmnmmn09@gmail.com>
2023-03-18 16:00:08 +0900

    added Echo.java

---
+++ xms/src/xms/Echo.java
@@ -0,0 +1,18 @@
+package xms;
+
+import java.util.Scanner;
+
+public class Echo {
+
+	public static void main(String[] args) {
+		String str;
+		
+		Scanner sc = new Scanner (System.in);
+		
+		str = sc.nextLine();
+		
+		System.out.println(str);
+
+	}
+
+}
