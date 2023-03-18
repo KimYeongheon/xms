c6c93097631d360bdda519913db46a0a4062e894
KimYeongheon <mmnmmnmmn09@gmail.com>
2023-03-18 14:34:02 +0900

    added menumanager.java

---
+++ xms/src/xms/MenuManager.java
@@ -0,0 +1,16 @@
+package xms;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+			System.out.println("1. Add Furnitures");
+			System.out.println("2. Delete Furnitures");
+			System.out.println("3. Edit Furnitures");
+			System.out.println("4. View Furnitures");
+			System.out.println("5. Exit");
+		
+	}
+
+}
