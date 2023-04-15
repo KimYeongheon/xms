001c66d4fa25fb7c5e7e568a7bd24adf07eb532b
KimYeongheon <mmnmmnmmn09@gmail.com>
2023-04-15 19:35:08 +0900

    added CarSimulator

---
+++ xms/src/CarSimulator.java
@@ -0,0 +1,11 @@
+
+public class CarSimulator {
+    public static void main(String[]args){
+        Car c1=new Car(2011,"KIA",80.2);
+        c1.println();
+        Car c2=new Car(2016,"HONDA",100);
+        c2.println();
+        Car c3=new Car(2022,"TESLA",140.2);
+        c3.println();
+    }
+}
