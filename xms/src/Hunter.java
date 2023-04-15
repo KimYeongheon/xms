7f58ab42d5af892553c5fea499dafd223351bc14
KimYeongheon <mmnmmnmmn09@gmail.com>
2023-04-15 19:35:31 +0900

    added Hunter

---
+++ xms/src/Hunter.java
@@ -0,0 +1,11 @@
+
+public class Hunter {
+    private int power;
+
+    Hunter(int power){
+        this.power = power;
+    }
+    public void attack(){
+        System.out.printf("Hunter attacked Monster with a power of %d.\n",power);
+    }
+}
