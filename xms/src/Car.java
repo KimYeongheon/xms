21f6ff43a62a082bfa2b89957c5392513eef5d01
KimYeongheon <mmnmmnmmn09@gmail.com>
2023-04-15 19:34:46 +0900

    added Car

---
+++ xms/src/Car.java
@@ -0,0 +1,26 @@
+
+public class Car {
+	int year;
+    String maker;
+    double speed;
+
+    public Car(int year,String maker,double speed){
+        this.year=year;
+        this.maker=maker;
+        this.speed=speed;
+    }
+    public int getYear(){
+        return year;
+    }
+    public String getMake(){
+        return maker;
+    }
+    public double getSpeed(){
+        return speed;
+    }
+    public void println() {
+    	System.out.print("출시년도 : " + getYear() + "년 ");
+    	System.out.print("제조사 : " + getMake() + " ");
+    	System.out.println("최고속력 : " + getSpeed() + " ");
+    }
+}
